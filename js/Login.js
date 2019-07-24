2f310d98d7344be76e05ba34e91b9c1ab6bde58e
Willjunger <41344969+Willjunger@users.noreply.github.com>
2019-07-23 22:57:16 -0300

    Comitando o js do login

--- js/Login.js
+++ js/Login.js
@@ -1,65 +1,121 @@
-var botaoAdm = document.querySelector("#login-adm1");
-var botaoFuncionario = document.querySelector("#login-funcionario1");
+var botaoAdm = document.querySelector("#login-adm-principal");
+var botaoFuncionario = document.querySelector("#login-funcionario-principal");
+
 var botaoVoltarAdm = document.querySelector("#btn-voltar-login-adm");
-var botaoLoginAdm = document.querySelector("#btn-entrar-login-adm");
 var botaoVoltarFuncionario = document.querySelector("#btn-voltar-login-funcionario");
-var botaoLoginFuncionario = document.querySelector("#btn-entrar-login-funcionario");
 
 
+// AÇÕES DO ADMINISTRADOR
+var loginFuncionarioPrincipal = document.getElementById("login-funcionario-principal");
+var loginAdmSecundario = document.getElementById("login-adm-secundario");
 
 botaoAdm.addEventListener("click", function(event) {
-	event.preventDefault();
-
-	var loginFuncionario1 = document.getElementById("login-funcionario1");
-	var loginAdm2 = document.getElementById("login-adm2");
-
-    if (loginFuncionario1.classList.contains('d-none')) {
-        loginFuncionario1.classList.remove("d-none");
-        loginAdm2.classList.add("d-none");
-    } else {
-        loginFuncionario1.classList.add("d-none");
-        loginAdm2.classList.remove("d-none");
-    }
-
+    event.preventDefault();
+    MostraLoginAdm();
 });
 
 botaoVoltarAdm.addEventListener("click", function(event) {
-    event.preventDefault();
-    var loginFuncionario1 = document.getElementById("login-funcionario1");
-	var loginAdm2 = document.getElementById("login-adm2");
-	loginFuncionario1.classList.remove("d-none");
-    loginAdm2.classList.add("d-none");
-});
-
-
-
-
-
-
-
-botaoFuncionario.addEventListener("click", function(event) {
 	event.preventDefault();
-
-	var loginFuncionario2 = document.getElementById("login-funcionario2");
-	var loginAdm1 = document.getElementById("login-adm1");
-
-	if (loginFuncionario2.classList.contains('d-none')) {
-        loginFuncionario2.classList.remove("d-none");
-        loginAdm1.classList.add("d-none");
-    } else {
-        loginFuncionario2.classList.add("d-none");
-        loginAdm1.classList.remove("d-none");
-    }
-});
-
-botaoVoltarFuncionario.addEventListener("click", function(event) {
-    event.preventDefault();
-    var loginFuncionario2 = document.getElementById("login-funcionario2");
-	var loginAdm1 = document.getElementById("login-adm1");
-
-    loginFuncionario2.classList.add("d-none");
-    loginAdm1.classList.remove("d-none");
+    MostraLoginAdmBtnVoltar();
 	
 });
 
+function MostraLoginAdm() {
+	if (loginFuncionarioPrincipal.classList.contains("d-none")) {
+		loginFuncionarioPrincipal.classList.remove("d-none");
+		loginAdmSecundario.classList.add("d-none");
+	} else {
+		loginFuncionarioPrincipal.classList.add("d-none");
+		loginAdmSecundario.classList.remove("d-none");
+	}
+}
 
+function MostraLoginAdmBtnVoltar(){
+    loginFuncionarioPrincipal.classList.remove("d-none");
+	loginAdmSecundario.classList.add("d-none");
+}
+
+
+
+
+// AÇÕES DO FUNCIONÁRIO
+var loginFuncionarioSecundario = document.getElementById("login-funcionario-secundario");
+var loginAdmPrincipal = document.getElementById("login-adm-principal");
+
+botaoFuncionario.addEventListener("click", function(event) {
+	event.preventDefault();
+	MostraLoginFuncionario();
+});
+
+botaoVoltarFuncionario.addEventListener("click", function(event) {
+	event.preventDefault();
+	MostraLoginFuncionarioBtnVoltar();
+});
+
+function MostraLoginFuncionario() {
+	if (loginFuncionarioSecundario.classList.contains("d-none")) {
+		loginFuncionarioSecundario.classList.remove("d-none");
+		loginAdmPrincipal.classList.add("d-none");
+	} else {
+		loginFuncionarioSecundario.classList.add("d-none");
+		loginAdmPrincipal.classList.remove("d-none");
+	}
+}
+
+function MostraLoginFuncionarioBtnVoltar() {
+	loginFuncionarioSecundario.classList.add("d-none");
+	loginAdmPrincipal.classList.remove("d-none");
+}
+
+
+// VERIFICAÇÃO DO LOGIN ADM
+
+var usuarioAdm = 'teste';
+var senhaAdm = 'teste123';
+
+var inputUserAdm = document.querySelector("#input-user-adm");
+var inputSenhaAdm = document.querySelector("#input-senha-adm");
+
+
+var botaoEntrarAdm = document.querySelector("#btn-entrar-login-adm");
+
+botaoEntrarAdm.addEventListener('click',function(event){
+    event.preventDefault();
+    
+    if(usuarioAdm !== inputUserAdm.value){
+        alert("Usuario inválido");
+        inputUserAdm.value = '';
+    }
+    
+    if(senhaAdm !== inputSenhaAdm.value){
+        alert("Senha Inválida");
+        inputSenhaAdm.value = '';
+    }
+
+
+});
+
+// VERIFICAÇÃO DO LOGIN FUNCIONARIO
+
+var usuarioFuncionario = 'func';
+var senhaFuncionario = 'func123';
+
+var inputUserFuncionario = document.querySelector("#input-user-funcionario");
+var inputSenhaFuncionario = document.querySelector("#input-senha-funcionario");
+
+var botaoEntrarFuncionario = document.querySelector("#btn-entrar-login-funcionario");
+
+botaoEntrarFuncionario.addEventListener("click", function(){
+    event.preventDefault();
+    
+
+    if(usuarioFuncionario !== inputUserFuncionario.value){
+        alert("Usuario inválido");
+        inputUserFuncionario.value = '';
+    }
+
+    if (senhaFuncionario !== inputSenhaFuncionario.value){
+        alert("Senha Inválida");
+        inputSenhaFuncionario.value = '';
+    }
+});
